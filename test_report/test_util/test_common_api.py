2b3acf805edc4859f7549e9a42e33c6ffa9c280d
anfreshman <303014439@qq.com>
2022-03-12 22:44:29 +0800

    update

--- test_report/test_util/test_common_api.py
+++ test_report/test_util/test_common_api.py
@@ -1,8 +1,26 @@
 import pytest
-class TestCommon:
+import requests
+import allure
 
-    def test_common_request(self):
-        assert 1 == 1
+
+class TestCommon:
+    # 全局变量env，用于读取当前的测试用例的请求参数
+    env = {}
+    # request_param是一个字典类型的变量
+    # 参数包括method,url,headers,bodys,params,except(dict类型)
+    def test_common_request(self, reqeust_param):
+        if(reqeust_param["method"] == "GET"):
+            timeout = 10
+            if(reqeust_param.get("timeout")):
+                timeout = reqeust_param.get("timeout")
+            re = requests.request(reqeust_param["method"],
+                                  url=reqeust_param["method"],
+                                  headers=reqeust_param["headers"],
+                                  params=reqeust_param["params"],
+                                  )
+            for flag in reqeust_param.get("except"):
+                assert flag == re.get(flag)
+
 
 if __name__ == "__main__":
     pytest.main([])
