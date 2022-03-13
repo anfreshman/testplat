344a00ea44ee09cd094b24f9ee6fc1873c608922
anfreshman <303014439@qq.com>
2022-03-13 09:40:14 +0800

    allure update

--- test_report/test_util/test_common_api.py
+++ test_report/test_util/test_common_api.py
@@ -8,18 +8,19 @@ class TestCommon:
     env = {}
     # request_param是一个字典类型的变量
     # 参数包括method,url,headers,bodys,params,except(dict类型)
-    def test_common_request(self, reqeust_param):
-        if(reqeust_param["method"] == "GET"):
-            timeout = 10
-            if(reqeust_param.get("timeout")):
-                timeout = reqeust_param.get("timeout")
-            re = requests.request(reqeust_param["method"],
-                                  url=reqeust_param["method"],
-                                  headers=reqeust_param["headers"],
-                                  params=reqeust_param["params"],
-                                  )
-            for flag in reqeust_param.get("except"):
-                assert flag == re.get(flag)
+    def test_common_request(self):
+        assert 1 == 2
+        # if(reqeust_param["method"] == "GET"):
+        #     timeout = 10
+        #     if(reqeust_param.get("timeout")):
+        #         timeout = reqeust_param.get("timeout")
+        #     re = requests.request(reqeust_param["method"],
+        #                           url=reqeust_param["method"],
+        #                           headers=reqeust_param["headers"],
+        #                           params=reqeust_param["params"],
+        #                           )
+        #     for flag in reqeust_param.get("except"):
+        #         assert flag == re.get(flag)
 
 
 if __name__ == "__main__":
